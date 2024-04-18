6bdd9a8ccab9a9db970450a042813e40dbf68d77
roshneee mahato <roshneemahato@gmail.com>
2024-04-18 12:43:13 +1200

    main

---
+++ day.py
@@ -0,0 +1,16 @@
+def main():
+
+    user_choice = input("Enter your choice:")
+
+    if user_choice == "1":
+        print("Books currently available are:")
+        #print everything
+    elif user_choice =="2":
+        print("To borrow book")
+        request = input
+    elif user_choice =="3":
+        print("To return book")
+
+
+if __name__ == "__main__":
+    main()
